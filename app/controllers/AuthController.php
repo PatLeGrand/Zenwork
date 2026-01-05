849490c734d976166314c1a646ad4234ec127b0a
Patrick <aurelpatrickm@gmail.com>
2026-01-05 07:04:48 -0500

    feat: implement AuthController

---
+++ app/controllers/AuthController.php
@@ -0,0 +1,63 @@
+<?php
+    require_once __DIR__ . '/../model/User.php';
+    require_once __DIR__ . '/../utils/validation.php';
+
+
+    class AuthController{
+        private $userModel;
+
+        public function __construct(){
+            $this->userModel = new User();
+        }
+
+        function register(){
+            $errors = [];
+            $success = false;
+
+            if($_SERVER['REQUEST_METHOD'] == 'POST'){
+                $firstname = sanitizeInput($_POST['firstname'] ?? '');
+                $lastname = sanitizeInput($_POST['lastname'] ?? '');
+                $email = sanitizeInput($_POST['email'] ?? '');
+                $password = $_POST['password'] ?? '';
+
+                if(empty($firstname)){
+                    $errors['firstname'] = "Le prénom est obligatoire";
+                } else if(!validateName($firstname)){
+                    $errors['firstname'] = "Le prénom doit contenir au moins 2 lettres";
+                }
+
+                if(empty($lastname)){
+                    $errors['lastname'] = "Le nom est obligatoire";
+                } else if(!validateName($lastname)){
+                    $errors['lastname'] = "Le nom doit contenir au moins 2 lettres";
+                }
+
+                 if (empty($email)) {
+                    $errors['email'] = 'L\'email est requis';
+                } elseif (!validateEmail($email)) {
+                     $errors['email'] = 'L\'email est invalide';
+                } elseif ($this->userModel->emailExists($email)) {
+                     $errors['email'] = 'Cet email est déja utilisé';
+                }
+
+                if (empty($password)) {
+                    $errors['password'] = 'Le mot de passe est requis';
+                } elseif (!validatePassword($password)) {
+                    $errors['password'] = 'Le mot de passe doit contenir au moins 8 caractères';
+                }
+
+                if(empty($errors)){
+                    $result = $this->userModel->create($email, $firstname, $lastname, $password);
+                     if($result){
+                         $success = true;
+                     } else {
+                         $errors ['general'] = "Une erreur est survenue lors de l'enregistrement";
+                     }
+
+                }
+
+            }
+            return ['errors' => $errors, 'success' => $success];
+        }
+    }
+?>
