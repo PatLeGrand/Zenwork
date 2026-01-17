1eec2ccd688c5ffdc82e1824d76b2fe35768672c
Patrick <aurelpatrickm@gmail.com>
2026-01-17 18:19:59 -0500

    feat: user view style

--- public/seach.php
+++ public/seach.php
@@ -20,7 +20,23 @@ $result = $userModel->searchUsers($query);
 if (empty($result)) {
     echo '<p class="p-4 text-gray-500">Aucun résultat trouvé</p>';
 } else {
-    foreach ($result as $user) {
-
+    foreach ($results as $user) {
+        ?>
+        <div class="p-3 hover:bg-gray-50 border-b flex items-center justify-between">
+            <div class="flex items-center space-x-3">
+                <img src="https://ui-avatars.com/api/?name=<?= urlencode($user['first_name'] . ' ' . $user['last_name']) ?>&background=random&color=fff"
+                     class="w-10 h-10 rounded-full">
+                <div>
+                    <p class="font-medium text-gray-900">
+                        <?= htmlspecialchars($user['first_name'] . ' ' . $user['last_name']) ?>
+                    </p>
+                </div>
+            </div>
+            <a href="profile.php?id=<?= $user['id'] ?>"
+               class="text-blue-600 hover:text-blue-700 text-sm font-medium">
+                Voir profil
+            </a>
+        </div>
+        <?php
     }
 }
